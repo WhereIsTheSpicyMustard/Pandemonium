5d9166c736d0819edb4290d30dca1836897fc424
Levi <levivoth77@gmail.com>
2026-08-26 11:27:59 -0500

    Add range function. Improve documentation

--- pandemonium.c
+++ pandemonium.c
@@ -7,6 +7,16 @@
 
 #define REPORT_ERROR(X) fprintf(stderr, "%s | %s | %d\n", X, __FILE__, __LINE__)
 
+#define ROTL(a,b) (((a) << (b)) | ((a) >> (32 - (b))))
+#define QR(a, b, c, d) do {      \
+a += b; d ^= a; d = ROTL(d, 16); \
+c += d; b ^= c; b = ROTL(b, 12); \
+a += b; d ^= a; d = ROTL(d,  8); \
+c += d; b ^= c; b = ROTL(b,  7); \
+} while (0)
+
+#define ROUNDS 20
+
 static uint64_t splitmix_state = 0;
 static uint64_t xorshift_state = 1;
 
@@ -23,15 +33,21 @@ uint64_t pandemonium_splitmix64(void)
     return z ^ (z >> 31);
 }
 
-uint64_t pandemonium_splitmix64_ex(const uint64_t x)
+uint64_t pandemonium_splitmix64_next(uint64_t x)
 {
-    uint64_t z = (x + 0x9e3779b97f4a7c15ULL);
-    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9ULL;
-    z = (z ^ (z >> 27)) * 0x94d049bb133111ebULL;
-    return z ^ (z >> 31);
+    x = (x + 0x9e3779b97f4a7c15ULL);
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9ULL;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111ebULL;
+    return x ^ (x >> 31);
 }
 
 /*******************************************************/
+
+// initial seed must be nonzero
+void pandemonium_xorshift64star_set_state(const uint64_t x)
+{
+    xorshift_state = x;
+}
 
 uint64_t pandemonium_xorshift64star(void)
 {
@@ -41,16 +57,17 @@ uint64_t pandemonium_xorshift64star(void)
     return xorshift_state * 0x2545F4914F6CDD1DULL;
 }
 
-// initial seed must be nonzero
-void pandemonium_xorshift64star_set_state(const uint64_t x)
+uint64_t pandemonium_xorshift64star_next(uint64_t x)
 {
-    xorshift_state = x;
+    x ^= x >> 12;
+    x ^= x << 25;
+    x ^= x >> 27;
+    return x * 0x2545F4914F6CDD1DULL;
 }
 
 /*******************************************************/
 
-// returns a non-deterministic random 1 byte value
-uint8_t pandemonium_get_uint8(void)
+uint8_t pandemonium_csprng_uint8(void)
 {
     uint8_t buf;
 
@@ -62,8 +79,7 @@ uint8_t pandemonium_get_uint8(void)
     return buf;
 }
 
-// returns a non-deterministic random 2 byte value
-uint16_t pandemonium_get_uint16(void)
+uint16_t pandemonium_csprng_uint16(void)
 {
     uint16_t buf;
 
@@ -75,8 +91,7 @@ uint16_t pandemonium_get_uint16(void)
     return buf;
 }
 
-// returns a non-deterministic random 4 byte value
-uint32_t pandemonium_get_uint32(void)
+uint32_t pandemonium_csprng_uint32(void)
 {
     uint32_t buf;
 
@@ -88,8 +103,7 @@ uint32_t pandemonium_get_uint32(void)
     return buf;
 }
 
-// returns a non-deterministic random 8 byte value
-uint64_t pandemonium_get_uint64(void)
+uint64_t pandemonium_csprng_uint64(void)
 {
     uint64_t buf;
 
@@ -101,7 +115,9 @@ uint64_t pandemonium_get_uint64(void)
     return buf;
 }
 
-uint8_t* pandemonium_get_uint8_arr(const size_t count)
+/*******************************************************/
+
+uint8_t* pandemonium_csprng_uint8_arr(const size_t count)
 {
     uint8_t* buf = malloc(count * sizeof(*buf));
     if (buf == NULL) {
@@ -117,7 +133,7 @@ uint8_t* pandemonium_get_uint8_arr(const size_t count)
     return buf;
 }
 
-uint16_t* pandemonium_get_uint16_arr(const size_t count)
+uint16_t* pandemonium_csprng_uint16_arr(const size_t count)
 {
     uint16_t* buf = malloc(count * sizeof(*buf));
 
@@ -134,7 +150,7 @@ uint16_t* pandemonium_get_uint16_arr(const size_t count)
     return buf;
 }
 
-uint32_t* pandemonium_get_uint32_arr(const size_t count)
+uint32_t* pandemonium_csprng_uint32_arr(const size_t count)
 {
     uint32_t* buf = malloc(count * sizeof(*buf));
 
@@ -151,7 +167,7 @@ uint32_t* pandemonium_get_uint32_arr(const size_t count)
     return buf;
 }
 
-uint64_t* pandemonium_get_uint64_arr(const size_t count)
+uint64_t* pandemonium_csprng_uint64_arr(const size_t count)
 {
     uint64_t* buf = malloc(count * sizeof(*buf));
 
@@ -168,7 +184,13 @@ uint64_t* pandemonium_get_uint64_arr(const size_t count)
     return buf;
 }
 
-// returns a random double in [0,1)
+/*******************************************************/
+
+uint64_t pandemonium_get_range(const uint64_t min, const uint64_t max)
+{
+    return min + (pandemonium_csprng_uint64() % (max - min));
+}
+
 double pandemonium_get_double(void)
 {
     uint64_t buf = ~(1ULL);
@@ -183,16 +205,6 @@ double pandemonium_get_double(void)
 }
 
 /*******************************************************/
-
-#define ROTL(a,b) (((a) << (b)) | ((a) >> (32 - (b))))
-#define QR(a, b, c, d) do {      \
-a += b; d ^= a; d = ROTL(d, 16); \
-c += d; b ^= c; b = ROTL(b, 12); \
-a += b; d ^= a; d = ROTL(d,  8); \
-c += d; b ^= c; b = ROTL(b,  7); \
-} while (0)
-
-#define ROUNDS 20
 
 static void pandemonium_chacha_block(uint32_t out[16], uint32_t const in[16])
 {
--- pandemonium.h
+++ pandemonium.h
@@ -4,24 +4,33 @@
 #include <stdint.h>
 #include <stddef.h>
 
-uint8_t   pandemonium_get_uint8 (void);
-uint16_t  pandemonium_get_uint16(void);
-uint32_t  pandemonium_get_uint32(void);
-uint64_t  pandemonium_get_uint64(void);
-
-uint8_t*  pandemonium_get_uint8_arr (const size_t count);
-uint16_t* pandemonium_get_uint16_arr(const size_t count);
-uint32_t* pandemonium_get_uint32_arr(const size_t count);
-uint64_t* pandemonium_get_uint64_arr(const size_t count);
-
-double    pandemonium_get_double(void);
-
+// These are non-secure psudo random number generators.
+// Good enough for many applications where security is
+// not a concern.
 void      pandemonium_splitmix64_set_state(const uint64_t x);
 uint64_t  pandemonium_splitmix64(void);
-uint64_t  pandemonium_splitmix64_ex(const uint64_t x);
-
-uint64_t  pandemonium_xorshift64star(void);
+uint64_t  pandemonium_splitmix64_next(uint64_t x);
 void      pandemonium_xorshift64star_set_state(const uint64_t x);
+uint64_t  pandemonium_xorshift64star(void);
+uint64_t  pandemonium_xorshift64star_next(uint64_t x);
 
+
+// these are cryptrographically secure psudo random number
+// generators that return a random value of the specified type.
+// The _arr versions return an array of length count of csprn 
+// of the specified type, allocated on the heap, thus they need
+// to be freed manually by free()
+uint8_t   pandemonium_csprng_uint8(void);
+uint16_t  pandemonium_csprng_uint16(void);
+uint32_t  pandemonium_csprng_uint32(void);
+uint64_t  pandemonium_csprng_uint64(void);
+uint8_t*  pandemonium_csprng_uint8_arr(const size_t count);
+uint16_t* pandemonium_csprng_uint16_arr(const size_t count);
+uint32_t* pandemonium_csprng_uint32_arr(const size_t count);
+uint64_t* pandemonium_csprng_uint64_arr(const size_t count);
+
+
+uint64_t  pandemonium_get_range(const uint64_t min, const uint64_t max); // returns a random int in [min, max), uses mod and therefore will contain mod bias
+double    pandemonium_get_double(void); // returns a random double in [0,1)
 
 #endif
